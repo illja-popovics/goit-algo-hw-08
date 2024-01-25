fa4cec9c5ebaed1a7a389959e3c6cd85c1666a82
illja-popovics <143592959+illja-popovics@users.noreply.github.com>
2024-01-25 22:03:32 +0100

    refactoring.

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 import heapq
 
 def min_cost_to_connect_cables(cable_lengths):
-    # Створюємо міні-купу, використовуючи наявний список кабелів
     heapq.heapify(cable_lengths)
     
-    total_cost = 0  # Загальні витрати
+    total_cost = 0  
     
-    # Об'єднуємо кабелі, доки не залишиться тільки один
     while len(cable_lengths) > 1:
-        # Беремо два найменших кабелі
         cable1 = heapq.heappop(cable_lengths)
         cable2 = heapq.heappop(cable_lengths)
         
-        # Обчислюємо витрати на їх об'єднання
         connection_cost = cable1 + cable2
         
-        # Додаємо витрати до загальних
         total_cost += connection_cost
         
-        # Додаємо новий кабель до купи
         heapq.heappush(cable_lengths, connection_cost)
     
     return total_cost
